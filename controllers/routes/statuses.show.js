7aaca408abd3f6347734985462579e9bc0fc33e3
Andrew Betts <andrew.betts@ft.com>
2013-10-11 16:19:49 +0100

    Rename to new naming convention, add memcached caching

--- controllers/routes/statuses.show.js
+++ controllers/routes/statuses.show.js
@@ -1,22 +1,38 @@
 
 module.exports = function(req, res) {
-	var format;
+	var format, mckey, deferred = require('deferred'), Memcached = require('memcached');
+
 	if (!req.query.id) throw('No twitter status id specified');
 
 	format = req.params.format.match(/^html|json$/) ? req.params.format : 'html';
-	require('../sources/twitterapi.js').statuses.show({
-		id: req.query.id,
-		format: req.params.format || 'html',
-		success: function(op) {
-			res.set('Cache-control', 'max-age=3600, public');
-			if (format == 'json') {
-				res.jsonp(op);
-			} else {
-				res.send(op);
-			}
-		},
-		error: function(msg) {
-			res.send(msg, 500);
+	mckey = 'origami-tweet-'+req.query.id+'-'+format;
+
+	def = deferred();
+	memcached = new Memcached('127.0.0.1:11211', {timeout:50});
+	memcached.get(mckey, function(err, cacheres) {
+		if (cacheres) {
+			def.resolve(cacheres);
+		} else {
+			require('../sources/twitterapi.js').statuses.show({
+				id: req.query.id,
+				format: format,
+				success: function(op) {
+					memcached.set(mckey, op, 3600, function() {});
+					def.resolve(op);
+				},
+				error: function(msg) {
+					res.send(500, msg);
+				}
+			});
+		}
+	});
+
+	def.promise(function(op) {
+		res.set('Cache-control', 'max-age=3600, public');
+		if (format == 'json') {
+			res.jsonp(op);
+		} else {
+			res.send(op);
 		}
 	});
 }
