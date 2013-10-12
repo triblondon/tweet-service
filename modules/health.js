b9d00bfc59bdc31899166517523f76f08ac120d9
Andrew Betts <andrew.betts@ft.com>
2013-10-12 20:33:46 +0100

    Improve metrics, add healthcheck

---
+++ modules/health.js
@@ -0,0 +1,39 @@
+var deferred = require('deferred'),
+_ = require('lodash'),
+timeout = 1000;
+
+var checks = [
+	{
+		name: "Twitter API",
+		check: "Retrieve a tweet from Twitter's statuses.show API",
+		businessImpact: "The tweet service won't work. Tweets may not appear on sites that rely on the tweet service, though since most will likely be cached, the most likely impact is that embedding new tweets will not work.",
+		technicalImpact: "None",
+		panicGuide: "If it's because we've hit our rate limit, the only immediate option is to contact Twitter and ask for an increased limit.",
+		severity: 3,
+		func: function(def) {
+			require('./twitterapi.js').statuses.show({
+				id: '210462857140252672',
+				format: 'json',
+				success: function() { def.resolve(true); },
+				error: function(msg) { def.reject(msg); }
+			});
+		}
+	}
+];
+
+exports.get = function(callback) {
+	var key, promiselist = [], data = [];
+	deferred.map(checks, function(check) {
+		var def = deferred();
+		check.func(def);
+		setTimeout(function() { def.reject(new Error("Timeout: check did not complete within "+timeout+"ms")) }, timeout);
+		def.promise.done(function() {
+			data.push(_.extend({}, _.pick(check, ['name', 'check']), {ok:true}));
+		}, function(msg) {
+			data.push(_.extend({}, _.pick(check, ['name', 'check', 'businessImpact', 'technicalImpact', 'panicGuide', 'severity']), {ok:false, checkOutput:JSON.stringify(msg)}));
+		});
+		return def.promise;
+	}).finally(function() {
+		callback(data);
+	})
+}
--- modules/metrics.js
+++ modules/metrics.js
@@ -1,63 +1,59 @@
 
-var loaded = false,
-metrics = {},
-deferred = require('deferred'),
+var deferred = require('deferred'),
 config = require('./config'),
-_ = require('lodash');
+_ = require('lodash'),
+timeout = 1000;
 
-metrics = {
-	twitterReachable: {
-		type: "boolean",
+var metrics = {
+	twitterRespTime: {
+		type: "count",
+		unit: "seconds",
 		func: function(def) {
 			var req = require('request');
-			req({
-				uri:'https://api.twitter.com/1.1/statuses/show.json?id=210462857140252672',
-				timeout: 500
-			}, function(err, resp, body) {
-				def.resolve(err ? {val:false} : {val:true});
+			var start = (new Date()).getTime();
+
+			// This won't actually work as it's not authenticated but this metric simply wants to know the response time
+			req.get('https://api.twitter.com/1.1/statuses/show.json?id=210462857140252672', function(err, resp, body) {
+				var end = (new Date()).getTime();
+				def.resolve(err ? null : (end-start)/1000);
 			});
 		}
 	},
-	memcacheReachable: {
-		type: "boolean",
+	memcacheRespTime: {
+		type: "count",
+		unit: "seconds",
 		func: function(def) {
-			var mcconfig = config.get('memcached');
 			var Memcached = require('memcached');
+			var mcconfig = config.get('memcached');
 			if (!mcconfig) {
-				def.resolve(undefined);
+				def.resolve(null);
 			} else {
+				var start = (new Date()).getTime();
 				memcached = new Memcached(mcconfig.servers, mcconfig.options);
-				memcached.items(function(err, data) {
-					def.resolve(err ? false : true);
+				memcached.get('test', function(err, data) {
+					var end = (new Date()).getTime();
+					def.resolve(err ? null : (end-start)/1000);
 				});
 			}
 		}
 	}
 }
 
-function load() {
-	var i, promiselist = [];
-	if (loaded) return deferred().resolve(metrics);
-	for (i in metrics) {
-		def = deferred();
-		metrics[i].func(def);
-		promiselist.push(def.promise);
-		def.promise(function(result) {
-			metrics[i] = _.extend({type: metrics[i].type}, result);
-			if (typeof result !== 'object') result = {val:result};
-			console.log('adding result', i, result);
-		})
+exports.get = function(callback) {
+	var key, promiselist = [], data = {};
+	for (key in metrics) {
+		promiselist.push(function(_key) {
+			var def = deferred();
+			metrics[key].func(def);
+			setTimeout(function() { def.resolve(null) }, timeout);
+			def.promise.then(function(result) {
+				if (typeof result !== 'object' || result === null) result = {val:result};
+				data[_key] = _.extend({}, _.omit(metrics[_key], 'func'), result);
+			});
+			return def.promise;
+		}(key));
 	}
-
-	// TODO: This doesn't work!   How do you group promises?
-	return deferred(promiselist)(function(result) {
-		loaded = true;
-		console.log('Metrics loaded', result);
+	deferred.apply(null, promiselist).then(function() {
+		callback(data);
 	});
-}
-
-exports.get = function(key, callback) {
-	load()(function(result) {
-		console.log('get', result);
-	})
 }
