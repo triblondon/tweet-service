3885df132ffb8ab477ca0ff4ce924bd111a0a89e
Andrew Betts <andrew.betts@ft.com>
2013-12-30 14:44:25 +0000

    Source no longer required for docs endpoints

--- controllers/standard/info.js
+++ controllers/standard/info.js
@@ -3,7 +3,7 @@ module.exports = function(req, res, next) {
 
 	// Redirect to current version if no version is specified
 	if (!req.params.version) {
-		res.redirect('/v1/?source='+req.source);
+		res.redirect('/v1/');
 	} else if (req.params.version == 1) {
 		res.set('Cache-control', 'max-age=3600, public');
 		res.sendfile('views/info.html');
